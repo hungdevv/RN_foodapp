22c25cfe1214da3caa7567891edf620b8d348034
nguyenhung <hunghung0309@gmail.com>
2021-02-21 10:21:02 +0700

    fix-import-navigation

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const SearchBar = () => {
+    return(
+        <View style={styles.background}>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE'
+    }
+});
+
+export default SearchBar;
